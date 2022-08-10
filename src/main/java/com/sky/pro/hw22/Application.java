d28ac8461fd872d27f20f3ac9506338e7424c852
Artemxex <Artem.363636@gmail.com>
2022-08-10 20:11:54 +0300

    Initial commit

---
+++ src/main/java/com/sky/pro/hw22/Application.java
@@ -0,0 +1,13 @@
+package com.sky.pro.hw22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
